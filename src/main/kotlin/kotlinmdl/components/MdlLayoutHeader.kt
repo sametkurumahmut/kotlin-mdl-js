b7db7a24d7765423477c123ccdea4faacb9b9ac7
Samet Kurumahmut <samet.kurumahmut@gmail.com>
2018-02-03 22:28:35 +0300

    Added base class for mdl layout header comp

---
+++ src/main/kotlin/kotlinmdl/components/MdlLayoutHeader.kt
@@ -0,0 +1,20 @@
+package kotlinmdl.components
+
+import kotlinmdl.extensions.empty
+import kotlinmdl.extensions.toggleClass
+import kotlinx.html.dom.create
+import kotlinx.html.js.header
+import org.w3c.dom.HTMLElement
+import kotlin.browser.document
+
+abstract class MdlLayoutHeader(isTransparent: Boolean = false, classes: String = String.empty)
+    : MdlLayoutElement<HTMLElement>(document.create.header(ELEMENT_NAME), classes) {
+
+    open var isTransparent by this.toggleClass(TRANSPARENT_MODIFIER_NAME, isTransparent)
+
+    companion object {
+
+        const val ELEMENT_NAME = "mdl-layout__header"
+        const val TRANSPARENT_MODIFIER_NAME = "mdl-layout__header--transparent"
+    }
+}
