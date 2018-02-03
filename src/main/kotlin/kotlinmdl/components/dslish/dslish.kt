f3e5e0ec30cfe3d60550901ae1cd781a46204c04
Samet Kurumahmut <samet.kurumahmut@gmail.com>
2018-02-04 01:56:59 +0300

    Added mdl layout title DSL for mdl layout drawer comp

--- src/main/kotlin/kotlinmdl/components/dslish/dslish.kt
+++ src/main/kotlin/kotlinmdl/components/dslish/dslish.kt
@@ -1,6 +1,7 @@
 package kotlinmdl.components.dslish
 
 import kotlinmdl.components.MdlComponent
+import kotlinmdl.components.MdlLayoutDrawer
 import kotlinmdl.components.MdlLayoutTitle
 import kotlinmdl.extensions.empty
 import kotlinmdl.extensions.plus
@@ -11,6 +12,12 @@ fun <T : Element> MdlComponent<T>.layoutTitle(
         title: String,
         classes: String = String.empty,
         block: MdlLayoutTitle.() -> Unit = {}) = this + MdlLayoutTitle(title, classes).apply(block)
+
+fun MdlLayoutDrawer.layoutTitle(
+        title: String = String.empty,
+        classes: String = String.empty,
+        block: MdlLayoutTitle.() -> Unit = {})
+        = this + MdlLayoutTitle(title, classes).also { this.layoutTitle = it; it.block() }
 //endregion
 
 //region Element Extensions
