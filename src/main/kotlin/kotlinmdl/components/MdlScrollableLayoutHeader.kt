e47558b84813ba6662333ab6281f7346bb2820e5
Samet Kurumahmut <samet.kurumahmut@gmail.com>
2018-02-03 22:31:33 +0300

    Added mdl scrollable layout header comp

---
+++ src/main/kotlin/kotlinmdl/components/MdlScrollableLayoutHeader.kt
@@ -0,0 +1,16 @@
+package kotlinmdl.components
+
+import kotlinmdl.extensions.empty
+
+open class MdlScrollableLayoutHeader(isTransparent: Boolean = false, classes: String = String.empty)
+    : MdlLayoutHeader(isTransparent, classes) {
+
+    init {
+        this.element.classList.add(MODIFIER_NAME)
+    }
+
+    companion object {
+
+        const val MODIFIER_NAME = "mdl-layout__header--scroll"
+    }
+}
