f135984e7a6aacfbe977b006ecf4e5ca31746b58
Samet Kurumahmut <samet.kurumahmut@gmail.com>
2018-02-03 22:32:44 +0300

    Added mdl waterfall layout header comp

---
+++ src/main/kotlin/kotlinmdl/components/MdlWaterfallLayoutHeader.kt
@@ -0,0 +1,22 @@
+package kotlinmdl.components
+
+import kotlinmdl.extensions.empty
+import kotlinmdl.extensions.toggleClass
+
+open class MdlWaterfallLayoutHeader(
+        isTopHideable: Boolean = false,
+        isTransparent: Boolean = false,
+        classes: String = String.empty) : MdlLayoutHeader(isTransparent, classes) {
+
+    init {
+        this.element.classList.add(MODIFIER_NAME)
+    }
+
+    open var isTopHideable by this.toggleClass(HIDE_TOP_MODIFIER_NAME, isTopHideable)
+
+    companion object {
+
+        const val HIDE_TOP_MODIFIER_NAME = "mdl-layout__header--waterfall-hide-top"
+        const val MODIFIER_NAME = "mdl-layout__header--waterfall"
+    }
+}
