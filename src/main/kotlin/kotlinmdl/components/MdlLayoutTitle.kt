08f00f9b2bec82a5d560cccb7fce3f5a8929afe0
Samet Kurumahmut <samet.kurumahmut@gmail.com>
2018-02-03 22:52:44 +0300

    Added mdl layout title comp

---
+++ src/main/kotlin/kotlinmdl/components/MdlLayoutTitle.kt
@@ -0,0 +1,19 @@
+package kotlinmdl.components
+
+import kotlinmdl.extensions.empty
+import kotlinmdl.extensions.setTextContentOfThis
+import kotlinx.html.dom.create
+import kotlinx.html.js.span
+import org.w3c.dom.HTMLSpanElement
+import kotlin.browser.document
+
+open class MdlLayoutTitle(title: String, classes: String = String.empty)
+    : MdlLayoutElement<HTMLSpanElement>(document.create.span(BLOCK_NAME), classes) {
+
+    open var title by this.setTextContentOfThis(title)
+
+    companion object {
+
+        const val BLOCK_NAME = "mdl-layout-title"
+    }
+}
