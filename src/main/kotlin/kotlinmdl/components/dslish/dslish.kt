ee7d00e21786b452b581a9658ebd35eb0e5137a3
Samet Kurumahmut <samet.kurumahmut@gmail.com>
2018-02-10 19:41:06 +0300

    Added mdl layout comp related DSLs

--- src/main/kotlin/kotlinmdl/components/dslish/dslish.kt
+++ src/main/kotlin/kotlinmdl/components/dslish/dslish.kt
@@ -7,6 +7,9 @@ import kotlinx.html.ATarget
 import org.w3c.dom.Element
 
 //region Mdl Component Extensions
+fun <T : Element> MdlComponent<T>.layout(classes: String = kotlin.String.empty, block: MdlLayout.() -> Unit = {})
+        = this + kotlinmdl.components.MdlLayout(classes).apply(block)
+
 fun <T : Element> MdlComponent<T>.layoutSpacer(classes: String = String.empty, block: MdlLayoutSpacer.() -> Unit = {})
         = this + MdlLayoutSpacer(classes).apply(block)
 
@@ -17,6 +20,48 @@ fun <T : Element> MdlComponent<T>.layoutTitle(
 
 fun <T : Element> MdlComponent<T>.nav(classes: String = kotlin.String.empty, block: MdlLayoutNav.() -> Unit = {})
         = this + MdlLayoutNav(classes).apply(block)
+
+fun MdlLayout.content(
+        title: String = String.empty,
+        classes: String = String.empty,
+        body: MdlLayoutContent.() -> Unit = {})
+        = this + MdlLayoutContent(title, classes, { this@content.content = this; body() })
+
+fun MdlLayout.drawer(classes: String = String.empty, block: MdlLayoutDrawer.() -> Unit = {})
+        = this + MdlLayoutDrawer(classes).also { this.drawer = it; it.block() }
+
+fun MdlLayout.fixedDrawer(classes: String = String.empty, block: MdlLayoutDrawer.() -> Unit = {})
+        = this + MdlLayoutDrawer(classes).also { this.drawer = it; this.hasFixedDrawer = true; it.block() }
+
+fun MdlLayout.fixedHeader(
+        isSeamed: Boolean = false,
+        isTransparent: Boolean = false,
+        classes: String = String.empty,
+        block: MdlFixedLayoutHeader.() -> Unit = {})
+        = this + MdlFixedLayoutHeader(isSeamed, isTransparent, classes).also {
+    this.header = it
+    this.hasFixedHeader = true
+    it.block()
+}
+
+fun MdlLayout.scrollableHeader(
+        isTransparent: Boolean = false,
+        classes: String = String.empty,
+        block: MdlScrollableLayoutHeader.() -> Unit = {})
+        = this + MdlScrollableLayoutHeader(isTransparent, classes).also {
+    this.header = it
+    it.block()
+}
+
+fun MdlLayout.waterfallHeader(
+        isTopHideable: Boolean = false,
+        isTransparent: Boolean = false,
+        classes: String = String.empty,
+        block: MdlWaterfallLayoutHeader.() -> Unit = {})
+        = this + MdlWaterfallLayoutHeader(isTopHideable, isTransparent, classes).also {
+    this.header = it
+    it.block()
+}
 
 fun MdlLayoutDrawer.layoutTitle(
         title: String = String.empty,
@@ -46,6 +91,9 @@ fun MdlLayoutNav.externalLink(
 //endregion
 
 //region Element Extensions
+fun Element.mdlLayout(classes: String = String.empty, block: MdlLayout.() -> Unit = {})
+        = this + MdlLayout(classes).apply(block)
+
 fun Element.mdlLayoutSpacer(classes: String = String.empty, block: MdlLayoutSpacer.() -> Unit = {})
         = this + MdlLayoutSpacer(classes).apply(block)
 
