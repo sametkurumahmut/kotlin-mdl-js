7dbb5c01346da90d7b472dba55c2fbe7e38964c0
Samet Kurumahmut <samet.kurumahmut@gmail.com>
2018-02-24 15:54:32 +0300

    Added abstractions for mdl fixed layout header comp

--- src/main/kotlin/kotlinmdl/components/MdlFixedLayoutHeader.kt
+++ src/main/kotlin/kotlinmdl/components/MdlFixedLayoutHeader.kt
@@ -1,22 +1,18 @@
 package kotlinmdl.components
 
 import kotlinmdl.internal.extensions.empty
-import kotlinmdl.internal.extensions.toggleClassOfThis
 import kotlinx.html.dom.create
 import kotlinx.html.js.header
 import org.w3c.dom.HTMLElement
 import kotlin.browser.document
 
 open class MdlFixedLayoutHeader(
+        element: HTMLElement,
         isSeamed: Boolean = false,
         isTransparent: Boolean = false,
         classes: String = String.empty)
-    : MdlLayoutHeader<HTMLElement>(document.create.header(), isTransparent, classes) {
+    : MdlFixedLayoutHeaderBase<HTMLElement>(element, isSeamed, isTransparent, classes) {
 
-    open var isSeamed by this.toggleClassOfThis(SEAMED_MODIFIER_NAME, isSeamed)
-
-    companion object {
-
-        const val SEAMED_MODIFIER_NAME = "mdl-layout__header--seamed"
-    }
+    constructor(isSeamed: Boolean = false, isTransparent: Boolean = false, classes: String = String.empty)
+            : this(document.create.header(), isSeamed, isTransparent, classes)
 }
