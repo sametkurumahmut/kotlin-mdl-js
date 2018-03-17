66df30b969898389e6aa49afbd355e6276b91770
Samet Kurumahmut <samet.kurumahmut@gmail.com>
2018-03-17 22:53:03 +0300

    Added mdl mega footer social button comp

---
+++ src/main/kotlin/kotlinmdl/components/MdlMegaFooterSocialButton.kt
@@ -0,0 +1,23 @@
+package kotlinmdl.components
+
+import kotlinmdl.internal.extensions.empty
+import kotlinx.html.ButtonFormEncType
+import kotlinx.html.ButtonFormMethod
+import kotlinx.html.ButtonType
+import kotlinx.html.dom.create
+import kotlinx.html.js.button
+import org.w3c.dom.HTMLButtonElement
+import kotlin.browser.document
+
+open class MdlMegaFooterSocialButton(element: HTMLButtonElement, classes: String = String.empty)
+    : MdlComponent<HTMLButtonElement>(
+        element.apply { classList.add(IMdlMegaFooterSocialButton.ELEMENT_NAME) },
+        classes), IMdlMegaFooterSocialButton<HTMLButtonElement> {
+
+    constructor(
+            formEncType: ButtonFormEncType? = null,
+            formMethod: ButtonFormMethod? = null,
+            name: String? = null,
+            type: ButtonType? = null,
+            classes: String = String.empty) : this(document.create.button(formEncType, formMethod, name, type), classes)
+}
