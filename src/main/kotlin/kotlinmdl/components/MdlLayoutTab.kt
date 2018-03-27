d47e5fe7726341f53bd0d76e3170266a93c47efe
Samet Kurumahmut <samet.kurumahmut@gmail.com>
2018-03-27 20:49:36 +0300

    Added mdl layout tab comp

---
+++ src/main/kotlin/kotlinmdl/components/MdlLayoutTab.kt
@@ -0,0 +1,21 @@
+package kotlinmdl.components
+
+import kotlinmdl.internal.extensions.empty
+import kotlinx.html.dom.create
+import kotlinx.html.js.a
+import org.w3c.dom.HTMLAnchorElement
+import kotlin.browser.document
+
+open class MdlLayoutTab(
+        element: HTMLAnchorElement,
+        hrefId: String,
+        isActive: Boolean = false,
+        classes: String = String.empty) : MdlLayoutTabBase<HTMLAnchorElement>(element, isActive, classes) {
+
+    constructor(hrefId: String, isActive: Boolean = false, classes: String = String.empty)
+            : this(document.create.a(), hrefId, isActive, classes)
+
+    init {
+        element.href = "#$hrefId"
+    }
+}
