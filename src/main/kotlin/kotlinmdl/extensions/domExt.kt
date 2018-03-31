f6f8e98b9888f99a01e58440747976c8e7224320
Samet Kurumahmut <samet.kurumahmut@gmail.com>
2018-03-31 03:17:45 +0300

    Added plus operator overloading for element

--- src/main/kotlin/kotlinmdl/extensions/domExt.kt
+++ src/main/kotlin/kotlinmdl/extensions/domExt.kt
@@ -1,8 +1,11 @@
 package kotlinmdl.extensions
 
 import kotlinmdl.components.IMdlComponent
+import kotlinmdl.style.IMdlStyle
 import org.w3c.dom.Element
 import org.w3c.dom.Node
+
+operator fun Element.plus(style: IMdlStyle) = this.apply { classList.add(style.toString()) }
 
 operator fun <T : IMdlComponent<E>, E : Element> Node.plus(component: T) = component.also { this + it.element }
 
