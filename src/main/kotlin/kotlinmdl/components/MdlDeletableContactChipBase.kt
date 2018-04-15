ae33b64ae176519f5442c8a05fe573e031354c65
Samet Kurumahmut <samet.kurumahmut@gmail.com>
2018-04-15 22:35:40 +0300

    Added abstractions for mdl deletable contact chip comp

---
+++ src/main/kotlin/kotlinmdl/components/MdlDeletableContactChipBase.kt
@@ -0,0 +1,9 @@
+package kotlinmdl.components
+
+import kotlinmdl.internal.extensions.empty
+import org.w3c.dom.Element
+
+abstract class MdlDeletableContactChipBase<out T : Element> protected constructor(
+        element: T,
+        classes: String = String.empty)
+    : MdlBasicChipBase<T>(element, true, true, classes), IMdlDeletableContactChip<T>
