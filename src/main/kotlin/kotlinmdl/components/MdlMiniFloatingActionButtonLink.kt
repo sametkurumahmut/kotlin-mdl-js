a96ce240099883800c8cf945dd0aca777c75d075
Samet Kurumahmut <samet.kurumahmut@gmail.com>
2018-05-01 01:03:59 +0300

    Added mdl mini fab button link comp

---
+++ src/main/kotlin/kotlinmdl/components/MdlMiniFloatingActionButtonLink.kt
@@ -0,0 +1,28 @@
+package kotlinmdl.components
+
+import kotlinmdl.components.style.IMdlMaterialIcon
+import kotlinmdl.config.MdlConfig
+import kotlinmdl.internal.extensions.empty
+import kotlinmdl.style.IMdlButtonColor
+import kotlinx.html.dom.create
+import kotlinx.html.js.a
+import org.w3c.dom.Element
+import org.w3c.dom.HTMLAnchorElement
+import kotlin.browser.document
+
+open class MdlMiniFloatingActionButtonLink(
+        element: HTMLAnchorElement,
+        icon: IMdlMaterialIcon<Element>? = null,
+        buttonColor: IMdlButtonColor? = null,
+        hasRippleEffect: Boolean = MdlConfig.hasRippleEffect,
+        classes: String = String.empty)
+    : MdlMiniFloatingActionButtonBase<HTMLAnchorElement>(element, icon, buttonColor, hasRippleEffect, classes) {
+
+    constructor(
+            icon: IMdlMaterialIcon<Element>? = null,
+            href: String? = null,
+            target: String? = null,
+            hasRippleEffect: Boolean = MdlConfig.hasRippleEffect,
+            classes: String = String.empty)
+            : this(document.create.a(href, target), icon, null, hasRippleEffect, classes)
+}
